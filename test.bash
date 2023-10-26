8fa39f644375e57d94718c0276234845c8db4085
cogolow Mori <rfthnz723@gmail.com>
2023-10-26 12:50:22 +0900

    change

--- test.bash
+++ test.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Natsumi Mori
+# SPDX-License-Identifier: BSD-3-Clause
 
+ani (){
+        echo NG at Line $1
+        ret=1
+}
+
+ret=0
+
+## I/O TEST ##
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 14 ] || ani ${LINENO}
+[ "$ret" = 0 ] && echo OK
+
+exit $ret
